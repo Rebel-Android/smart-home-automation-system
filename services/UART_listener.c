f2266cd2928e242708da899682769fa6daaca25d
Andrew Popravka <dioxyphthalophenone@tutamail.com>
2026-07-01 23:55:48 +0300

    Add files via upload

---
+++ services/UART_listener.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+int main() {
+  int serial_port = open("/dev/ttyS5", O_RDWR);
+  struct termios tty;
+  if(tcgetattr(serial_port, &tty) != 0) {
+      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+      return 1;
+  }
+  tty.c_cflag &= ~PARENB;
+  tty.c_cflag &= ~CSTOPB;
+  tty.c_cflag &= ~CSIZE;
+  tty.c_cflag |= CS8;
+  tty.c_cflag &= ~CRTSCTS;
+  tty.c_cflag |= CREAD | CLOCAL;
+  tty.c_lflag &= ~ICANON;
+  tty.c_lflag &= ~ECHO;
+  tty.c_lflag &= ~ECHOE;
+  tty.c_lflag &= ~ECHONL;
+  tty.c_lflag &= ~ISIG;
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+  tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL);
+  tty.c_oflag &= ~OPOST;
+  tty.c_oflag &= ~ONLCR;
+  tty.c_cc[VTIME] = 10;
+  tty.c_cc[VMIN] = 0;
+  cfsetispeed(&tty, B57600);
+  cfsetospeed(&tty, B57600);
+  if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+      return 1;
+  }
+ 
+while (1) 
+  {
+  char read_buf [512];
+  memset(&read_buf, '\0', sizeof(read_buf));
+  int num_bytes = read(serial_port, &read_buf, sizeof(read_buf));
+  if (num_bytes < 0)
+      {
+      printf("Error reading: %s", strerror(errno));
+      return 1;
+      }
+  if (num_bytes !=0 ) 
+      {
+      printf("%s", read_buf); 
+      system(read_buf);
+      num_bytes = 0;
+      }
+  }  
+  
+}
